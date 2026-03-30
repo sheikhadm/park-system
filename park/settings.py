9e9a16d49490291a4e0aefbcb1b88fddf25744c2
Abdullahi <abkid04@gmail.com>
2026-03-30 14:56:18 +0100

    added host

--- park/settings.py
+++ park/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$x6=ph*_4is6b^mxw8x-&hx(#(eeu3r#94sw&4peu+b+(%cd(r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
